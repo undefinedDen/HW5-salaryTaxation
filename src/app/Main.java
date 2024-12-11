750f60c57a5eb50214c138f6fcc6080e1df3a4bf
undefinedDen <tunevdenis123@gmail.com>
2024-12-11 10:54:32 +0200

    Update the program. Add new variable "taxPercent". Add still one block if - else for selection of tax percent. Add new variable to calculate.

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,31 +7,40 @@ public class Main {
     static double salary;
     static double salaryAfterTax;
     public static final String CURRENCY = "USD";
+    static double taxPercent;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter only numbers!!!! \nEnter salary, USD: ");
         salary = scanner.nextDouble();
+
         if (salary > 0 && salary <= 10000) {
-            tax = (salary * 2.5) / 100;
+            taxPercent = 2.5;
+        } else if (salary > 10000 && salary <= 25000) {
+            taxPercent = 4.3;
+        } else {
+            taxPercent = 6.7;
+        }
+        if (salary > 0 && salary <= 10000) {
+            tax = (salary * taxPercent) / 100;
             salaryAfterTax = salary - tax;
             System.out.printf("You enter salary: %.2f %s \n This month you paid %.2f %s into the country's treasury," +
                     "%n that's good!! " +
-                    "\nYour income was %.2f %s",salary, CURRENCY, tax, CURRENCY, salaryAfterTax,CURRENCY);
-        }else if(salary > 10000 && salary <= 25000){
-            tax = (salary * 4.3)/ 100;
+                    "\nYour income was %.2f %s", salary, CURRENCY, tax, CURRENCY, salaryAfterTax, CURRENCY);
+        } else if (salary > 10000 && salary <= 25000) {
+            tax = (salary * taxPercent) / 100;
             salaryAfterTax = salary - tax;
             System.out.printf("You enter salary: %.2f %s \n This month you paid %.2f %s into the country's treasury," +
                     "%n that's very good!! " +
-                    "\nYour income was %.2f %s",salary, CURRENCY, tax, CURRENCY, salaryAfterTax,CURRENCY);
-        }else if (salary >25000){
-            tax = (salary * 6.7)/ 100;
+                    "\nYour income was %.2f %s", salary, CURRENCY, tax, CURRENCY, salaryAfterTax, CURRENCY);
+        } else if (salary > 25000) {
+            tax = (salary * taxPercent) / 100;
             salaryAfterTax = salary - tax;
             System.out.printf("You enter salary: %.2f %s\n This month you paid %.2f %s into the country's treasury," +
                     "%n that's excellent!! " +
-                    "\nYour income was %.2f %s",salary, CURRENCY, tax, CURRENCY, salaryAfterTax,CURRENCY);
-        }else{
+                    "\nYour income was %.2f %s", salary, CURRENCY, tax, CURRENCY, salaryAfterTax, CURRENCY);
+        } else {
             System.out.println("Oh, it is very bad, you did not paid your tax this month. \n You must find a work!!!");
         }
     }
