d1fea3d34c69544b6c88da390d68ad86af4c53eb
undefinedDen <tunevdenis123@gmail.com>
2024-12-11 09:55:00 +0200

    Declaration of variables

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,14 @@
 package app;
-import java.util.Scanner;
-public class Main {
 
-    public static void main(String[] args){
-     Scanner scanner = new Scanner(System.in);
+import java.util.Scanner;
+
+public class Main {
+    static double tax;
+    static double salary;
+    static double salaryAfterTax;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
     }
 }
